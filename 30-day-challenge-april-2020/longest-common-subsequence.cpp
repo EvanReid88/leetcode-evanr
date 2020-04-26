6ec899673505beb928a86984d68d1d654b5c6edf
Evan Reid <evan@reids.us>
2020-04-26 14:24:00 -0700

    Longest common subsequence solution

---
+++ 30-day-challenge-april-2020/longest-common-subsequence.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/longest-common-subsequence/
+
+// dynamic programming approach
+// O(MN) Time
+// O(MN) Space
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int dp[len1 + 1][len2 + 1];
+    
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+                else if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[len1][len2];
+    }
+};
